29cdc9cd48a63c49bd0ffffedf854c97595b0975
winarno12 <narnowin0407@gmail.com>
2022-10-27 12:49:15 +0700

    fix footer

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,6 +1,6 @@
 			<footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between">
-				<p class="text-muted text-center text-md-left">Copyright © 2020 <a href="" target="_blank">NobleUI</a>. All rights reserved</p>
-				<p class="text-muted text-center text-md-left mb-0 d-none d-md-block">Handcrafted With <i class="mb-1 text-primary ml-1 icon-small" data-feather="heart"></i></p>
+				<p class="text-muted text-center text-md-left">Copyright © 2020 <a href="" target="_blank">Perdin App</a>. All rights reserved</p>
+				<p class="text-muted text-center text-md-left mb-0 d-none d-md-block">winarno<i class="mb-1 text-primary ml-1 icon-small" data-feather="heart"></i></p>
 			</footer>
 		</div>
 	</div>
